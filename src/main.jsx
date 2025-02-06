6469cea85b9110a5356fff592bab7b1ab3020ab6
Lisa Benini <lisabenini11@gmail.com>
2025-02-06 13:37:24 +0100

    Primo commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from './Home';
+
+const root = createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: Home
+    }
+]);
+
+root.render( <RouterProvider router={router} />);
