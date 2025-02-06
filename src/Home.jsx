19910c559f4fa5fee4c7aed92bd376f58629fce9
Lisa Benini <lisabenini11@gmail.com>
2025-02-06 14:18:37 +0100

    inizio Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 const Home = () => {
     return (
-        <div>
-            <h2>Ciao</h2>
+        <div className="home-page">
+            <div className="header">
+                <h2><span style={{color: "#FF6767"}}>TO</span>-DO</h2>
+            </div>
+            <div className="site-content">
+                <Sidebar />
+            </div>
         </div>
     )
 }
